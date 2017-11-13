67e2a8ed0a1a0cba733fb60efd2d12dd5bc5ab78
hdknr <gmail@hdknr.com>
2017-11-13 21:07:49 +0900

    /bv/ for bootstrap-vue

--- web/app/urls.py
+++ web/app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.static import serve
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('bv.urls')),           # bootstrap-vue sample
+    url(r'^bv/', include('bv.urls')),           # bootstrap-vue sample
 ]

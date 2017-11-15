028c3c78fec1a026bfd7d205f916fefd8638d758
hdknr <gmail@hdknr.com>
2017-11-15 11:23:54 +0900

    bootstrap-vue bsse layout

--- web/bv/urls.py
+++ web/bv/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^howto/(?P<name>.+)', views.howto, name="vue_howto"),
     url(r'(?P<component>.+)', views.component, name="vue_component"),
     url(r'^$', views.index),
 ]
--- web/bv/views.py
+++ web/bv/views.py
@@ -12,3 +12,9 @@ def component(request, component):
     template_name = 'bootstrap-vue/{}/index.html'.format(component)
     return TemplateResponse(
         request, template_name, context=dict(request=request))
+
+
+def howto(request, name):
+    template_name = 'bootstrap-vue/howto/{}/index.html'.format(name)
+    return TemplateResponse(
+        request, template_name, context=dict(request=request))

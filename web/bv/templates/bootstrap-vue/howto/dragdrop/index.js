c745eba7b313a46d88baaed09d2da8a0743dd814
hdknr <gmail@hdknr.com>
2017-11-15 14:26:16 +0900

    drag'n drop a b-col

--- web/bv/templates/bootstrap-vue/howto/dragdrop/index.js
+++ web/bv/templates/bootstrap-vue/howto/dragdrop/index.js
@@ -2,7 +2,24 @@ Vue.options.delimiters = ['{[{', '}]}'];        // Django テンプレートと�
 var app = new Vue({
   el: '#app',
   data: {
+    callback: function(){},
+    drag: null,
+    visible: true,
+    mediafiles: [
+        {id: 0, data: 'test-0'},
+        {id: 1, data: 'test-1'},
+        {id: 2, data: 'test-2'},
+        {id: 3, data: 'test-3'},
+        {id: 4, data: 'test-4'},]
   },
   methods: {
+      on_dragstart(mediafile,e) {this.drag = mediafile; },
+      on_dragenter(mediafile, e){this.dragenter =  mediafile; },
+      on_drop(e) {
+        this.mediafiles.splice(
+            this.dragenter, 0,
+            this.mediafiles.splice(this.drag, 1)[0]);
+        this.callback();
+      },
   }
 });
